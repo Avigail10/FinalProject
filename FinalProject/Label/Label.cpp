f5b289e7a8940beb1c232361fa1516b4c4e25930
AdielPerez20 <adielperez20@gmail.com>
2018-07-26 18:43:14 +0300

    constructor and getters-Label

---
+++ FinalProject/Label/Label.cpp
@@ -0,0 +1,24 @@
+#include "Label.h"
+
+
+Label::Label(int width, string value):Control(width)
+{
+	isfocusable = false;
+	if (width - value.size() < 2) 
+		this->value = value;
+	else {
+		this->value = centerlizeTxt(width, value);
+	}
+}
+
+string Label::getValue() {
+	return value;
+}
+
+void Label::setValue(string val) {
+	value = centerlizeTxt(getWidth() - 2, val);
+}
+
+Label::~Label()
+{
+}
