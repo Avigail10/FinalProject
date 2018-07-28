886534d0cb61c6b64546e3c7c277b0b949ac5da7
AdielPerez20 <adielperez20@gmail.com>
2018-07-28 15:15:36 +0300

    CheckList-Final

--- FinalProject/CheckList/CheckList.cpp
+++ FinalProject/CheckList/CheckList.cpp
@@ -14,3 +14,33 @@ CheckList::CheckList(int height, int width, vector<string> options) : ListPanel(
 		i++;
 	}
 }
+vector<size_t> CheckList::getSelectedIndices(){
+    vector<size_t> selected;
+    getAllControls(controls);
+    int cSize = controls.size();
+    for (int i = 0; i < cSize; i++){
+        if (static_cast<ButtonItem*>(controls[i])->txtMarkedChecker()) selected.push_back(i);
+    }
+    return selected;
+}
+
+void CheckList::selectIndex(size_t index){
+    static_cast<ButtonItem*>(controls[index])->toggle();
+}
+
+void CheckList::deselectIndex(size_t index){
+    static_cast<ButtonItem*>(controls[index])->toggle();
+}
+
+void CheckList::onEnterKey(){
+    selectIndex(itemInFocus());
+}
+
+void CheckList::mousePressed(short x, short y, bool isLeft){
+    Control::mousePressed(x, y, isLeft);
+    
+    int cSize = controls.size();
+    for (int i = 0; i < cSize; i++) {
+        controls[i]->mousePressed(x, y, isLeft);
+    }
+}
