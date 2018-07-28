0cf24408f290282d20fc12cc0b41647efff946ab
AdielPerez20 <adielperez20@gmail.com>
2018-07-28 14:46:13 +0300

    CheckList-h&cpp-first

---
+++ FinalProject/CheckList/CheckList.cpp
@@ -0,0 +1,16 @@
+#include "CheckList.h"
+
+
+CheckList::CheckList(int height, int width, vector<string> options) : ListPanel(height, width, options){
+	int length = options.size();
+	int i = 0;
+	while (i < length) {
+		ButtonItem *btn = new ButtonItem(options[i], getWidth() - 4, i);
+		btn->setBorder(BorderType::Single);
+		SelectListener* listener = new SelectListener(*this);
+		btn->addListener(*listener);
+		addControl(btn, getBodyLeft(), getBodyTop() + (i * 3));
+
+		i++;
+	}
+}
